4ee1aec14fa7071237bea1df6abb705b3f85bcc6
iachieve <mhalim80@hotmail.com>
2020-04-03 08:57:19 -0700

    display message formatted time with location

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,9 +17,10 @@ socket.on('message', (message) => {
   $messages.insertAdjacentHTML('beforeend', html);
 });
 
-socket.on('locationMessage', url => {
+socket.on('locationMessage', message => {
   const html = Mustache.render(locationMessageTemplate, {
-    url: url
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
   });
   $messages.insertAdjacentHTML('beforeend', html);
 });
