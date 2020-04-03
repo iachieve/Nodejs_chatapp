ade883c743760c07bb8b66cf8bb53be440e1a09e
iachieve <mhalim80@hotmail.com>
2020-04-03 09:11:44 -0700

    fix bug in display message ui-template

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,6 +18,7 @@ socket.on('message', (message) => {
 });
 
 socket.on('locationMessage', message => {
+  console.log(message);
   const html = Mustache.render(locationMessageTemplate, {
     url: message.url,
     createdAt: moment(message.createdAt).format('h:mm a')
